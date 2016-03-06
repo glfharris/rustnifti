ee3866ab0ddb16516008262c00ae46b82306b136
George Harris <glfharris@gmail.com>
2016-03-06 23:12:57 +0000

    Added ftfreq

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,25 @@ use transform::ft::*;
 extern crate num;
 use num::Complex;
 
+extern crate gnuplot;
+use gnuplot::{Figure, Caption, Color};
+
+use std::iter::Iterator;
+
 fn main() {
-
-    let v = vec![0.0, 1.0,2.0,3.0,4.0,5.0, 0.0,1.0,2.0,3.0,4.0,5.0];
-    let t = dft(v);
-
-    for x in t {
-        println!("{:}", x.re);
+    let mut sam = Vec::new();
+    for x in 0..200 {
+        sam.push((x as f64 / 2.0).sin() + (x as f64 / 5.0).sin())
     }
+    let f = ftfreq(sam.len(), 0.067);
+    let a = dft(sam);
+    let mut amps = Vec::new();
+    for x in a {
+        amps.push(x.norm());
+    }
+
+    let mut fg = Figure::new();
+    fg.axes2d()
+      .lines(&f, &amps, &[Caption("A line"), Color("black")]);
+    fg.show();
 }
--- src/transform/ft.rs
+++ src/transform/ft.rs
@@ -4,6 +4,8 @@ use self::num::Complex;
 use std::f64::consts::PI;
 use std::iter::Iterator;
 
+/// A Discrete Fourier Transfom using a slow algorithm
+/// * `x` - Sample
 pub fn dft(x: Vec<f64>) -> Vec<Complex<f64>> {
     let len = x.len();
     let mut ft: Vec<Complex<f64>> = Vec::with_capacity(len);
@@ -17,4 +19,26 @@ pub fn dft(x: Vec<f64>) -> Vec<Complex<f64>> {
                  .fold(Complex::new(0f64, 0f64), |sum, t| sum + t))
     }
     ft
+}
+
+/// Returns the DFT sample frequencies
+/// * `n` - Length of sample
+/// * `t` - Sample spacing
+pub fn ftfreq(n: usize, d: f64) -> Vec<f64> {
+    let window = n as isize;
+    let val = 1.0 / (window as f64 * d);
+    let (plus, neg) = match window % 2 {
+        0 => (window / 2 - 1, -window / 2),
+        1 => ((window - 1) / 2, -(window - 1) / 2),
+        _ => panic!("Error"),
+    };
+    let mut result: Vec<f64> = Vec::with_capacity(n);
+
+    for x in 0..(plus + 1) {
+        result.push(x as f64 * val);
+    }
+    for x in neg..0 {
+        result.push(x as f64 * val);
+    }
+    result
 }
